73e4218dec43b6070d386c5f0df6eb27f91a3656
Houssem Nasri <housi.housi2015@gmail.com>
2021-12-09 18:28:52 +0100

    Add Day5 solution in Java

---
+++ Day05/Java/Part2.java
@@ -0,0 +1,60 @@
+package tech.houssemnasri;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Part2 {
+
+    public static void run(String inputPath) {
+        try {
+            Byte[][] pointsGrid = new Byte[1000][1000];
+            Files.readAllLines(Path.of(inputPath)).forEach(line -> {
+                String[] segmentPoints = line.split(" -> ");
+                String[] point1 = segmentPoints[0].split(",");
+                String[] point2 = segmentPoints[1].split(",");
+                int x1 = Integer.parseInt(point1[0]);
+                int y1 = Integer.parseInt(point1[1]);
+                int x2 = Integer.parseInt(point2[0]);
+                int y2 = Integer.parseInt(point2[1]);
+                //Vertical
+                if (x1 == x2) {
+                    IntStream.range(Math.min(y1, y2), Math.max(y1, y2) + 1).forEach(i -> {
+                        if (pointsGrid[i][x1] == null)
+                            pointsGrid[i][x1] = 0;
+                        pointsGrid[i][x1]++;
+                    });
+                } else if (y1 == y2) {
+                    IntStream.range(Math.min(x1, x2), Math.max(x1, x2) + 1).forEach(i -> {
+                        if (pointsGrid[y1][i] == null)
+                            pointsGrid[y1][i] = 0;
+                        pointsGrid[y1][i]++;
+                    });
+                } else {
+                    if (x1 - x2 == y1 - y2) {
+                        IntStream.range(0, Math.abs(x1 - x2) + 1).forEach(i -> {
+                            if (pointsGrid[Math.min(y1, y2) + i][Math.min(x1, x2) + i] == null)
+                                pointsGrid[Math.min(y1, y2) + i][Math.min(x1, x2) + i] = 0;
+                            pointsGrid[Math.min(y1, y2) + i][Math.min(x1, x2) + i]++;
+                        });
+                    } else {
+                        IntStream.range(0, Math.abs(x1 - x2) + 1).forEach(i -> {
+                            int maxX = Math.max(x1, x2);
+                            int minY = Math.min(y1, y2);
+
+                            if (pointsGrid[minY + i][maxX - i] == null)
+                                pointsGrid[minY + i][maxX - i] = 0;
+                            pointsGrid[minY + i][maxX - i]++;
+                        });
+                    }
+                }
+            });
+            System.out.println(Arrays.stream(pointsGrid).flatMap(Stream::of).filter(i -> i != null && i >= 2).count());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
